806666dc0cfe961439292616f15cce9f98c864d7
JotaDD <joaoeduardogomes@gmail.com>
2024-02-07 20:59:48 -0300

    lint: delete comma

--- src/components/Contact/wppMessage.js
+++ src/components/Contact/wppMessage.js
@@ -1,5 +1,5 @@
 export default function wppMessage(name) {
-  console.log(name);
+  console.log(name)
   const fullName = name.split(' ')
   if (!fullName) return name
   return fullName.reduce((prev, cur) => {

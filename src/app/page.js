1c1fd0e8111a986f7a1952a16529d5d4c9f9886f
JuanOros <juenas@hotmail.com>
2024-02-08 12:17:14 -0300

    :bug: fix lint

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import Contact from "@/components/Contact";
-import Image from "next/image";
+import Contact from '@/components/Contact'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+      <Header />
       <h1 className="text-4xl font-bold">Hello, World!</h1>
       <Contact />
     </main>
-  );
+  )
 }

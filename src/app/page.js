875e8d2a73bae759335ad868546dcc6a7ed855ba
JuanOros <juenas@hotmail.com>
2024-02-08 12:20:39 -0300

    :bug: vercel conflit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
 import Contact from '@/components/Contact'
-import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-24">
-      <Header />
       <h1 className="text-4xl font-bold">Hello, World!</h1>
       <Contact />
     </main>

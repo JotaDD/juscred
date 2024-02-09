906578ed84f09bab0bb016e0b1ce82c0b716a1e8
JuanOros <juenas@hotmail.com>
2024-02-09 16:35:06 -0300

    :bug: concerta linha cinza do All Rights

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,19 +7,21 @@ import GoldenLogo from './icons/GoldenLogo'
 function Footer() {
   return (
     <footer className="w-screen bg-white p-12 md:px-24 mt-10">
-      <h3 className="text-jc-dark-blue text-xl font-bold w-full text-center mb-8 hidden md:block">
-        Juscred Precatórios e Ativos Ltda.
-      </h3>
-      <div className="max-w-[1600px] m-auto grid grid-cols-1 gap-14 md:grid-cols-2 xl:gap-0 xl:grid-cols-3 items-end xl">
-        <div className="col-span-1">
-          <GoldenLogo />
+      <div className="max-w-[1600px] mx-auto">
+        <h3 className="text-jc-dark-blue text-xl font-bold w-full text-center mb-8 hidden md:block">
+          Juscred Precatórios e Ativos Ltda.
+        </h3>
+        <div className="max-w-[1600px] m-auto grid grid-cols-1 gap-14 md:grid-cols-2 xl:gap-0 xl:grid-cols-3 items-end xl">
+          <div className="col-span-1">
+            <GoldenLogo />
+          </div>
+          <Information />
+          <div className="md:col-span-2 xl:col-span-1">
+            <SocialNetwork />
+          </div>
         </div>
-        <Information />
-        <div className="md:col-span-2 xl:col-span-1">
-          <SocialNetwork />
-        </div>
+        <AllRights />
       </div>
-      <AllRights />
     </footer>
   )
 }

e0e3a7c045995c4e7adebb9b6f5da3fe71a70cce
JuanOros <juenas@hotmail.com>
2024-02-09 11:48:42 -0300

    :sparkles: adiciona itens do componente com responsividade

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Faq from '@/components/Faq'
 import Contact from '@/components/Contact'
+import HowWorks from '@/components/HowWorks'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <h1 className="text-4xl font-bold">Hello, World!</h1>
-      <Faq />
+      <HowWorks />
       <Contact />
     </main>
   )
--- src/components/HowWorks/index.jsx
+++ src/components/HowWorks/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Category from './Category'
+import Image from 'next/image'
 
-function index() {
-  return <div>index</div>
+function HowWorks() {
+  return (
+    <div className="w-screen bg-white p-12 md:p-24">
+      <div className="flex justify-start md:justify-around max-w-[1600px] m-auto items-center">
+        <div className="lg:w-4/6 items-center">
+          <h2 className="text-jc-dark-blue text-4xl pb-5">Como Funciona?</h2>
+          <Category />
+        </div>
+
+        <div className="max-w-96">
+          <Image
+            className="hidden lg:block ml-4 object-center object-cover"
+            src="/doc.png"
+            alt="document image"
+            width="350"
+            height="1"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default index
+export default HowWorks

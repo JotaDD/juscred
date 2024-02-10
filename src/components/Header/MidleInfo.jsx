3ecca955d46ace840096206496db723c2c15e9c2
JuanOros <juenas@hotmail.com>
2024-02-09 22:00:11 -0300

    :tada: inicia componente Header

---
+++ src/components/Header/MidleInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function MidleInfo() {
+  return <div>MidleInfo</div>
+}
+
+export default MidleInfo
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './NavBar'
+import MidleInfo from './MidleInfo'
+import FooterInfo from './FooterInfo'
+
+function Header() {
+  return (
+    <div>
+      <NavBar />
+      <MidleInfo />
+      <FooterInfo />
+    </div>
+  )
+}
+
+export default Header

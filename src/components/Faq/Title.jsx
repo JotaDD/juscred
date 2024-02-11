f44a06f7ec5491debe44c7e9e853799ab99e5902
JotaDD <joaoeduardogomes@gmail.com>
2024-02-11 15:37:05 -0300

    style: FAQ title

--- src/components/Faq/Title.jsx
+++ src/components/Faq/Title.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h2 className="text-4xl font-bold mb-8 text-center ">
+    <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center ">
       Perguntas Frequentes
     </h2>
   )

971c4a0de6308f4ca353dd34040f792c3ce8e707
JotaDD <joaoeduardogomes@gmail.com>
2024-02-11 15:36:44 -0300

    fix: + and - button fix

--- src/components/Faq/ButtonSVG.jsx
+++ src/components/Faq/ButtonSVG.jsx
@@ -1,30 +1,34 @@
 const ButtonSVG = ({ isOpen }) => {
   return (
-    <svg
-      className="fill-jc-dark-blue shrink-0 ml-8"
-      width="16"
-      height="16"
-      xmlns="http://www.w3.org/2000/svg"
+    <div
+      className={`p-2 rounded w-fit bg-jc-${isOpen ? 'dark-blue' : 'white'}`}
     >
-      <rect
-        y="7"
+      <svg
+        className={`${isOpen ? 'fill-jc-white' : 'fill-jc-dark-blue'} shrink-0`}
         width="16"
-        height="2"
-        rx="1"
-        className={`transform origin-center transition duration-500 ease-out ${
-          isOpen && '!rotate-180'
-        }`}
-      />
-      <rect
-        y="7"
-        width="16"
-        height="2"
-        rx="1"
-        className={`transform origin-center rotate-90 transition duration-500 ease-out ${
-          isOpen && '!rotate-180'
-        }`}
-      />
-    </svg>
+        height="16"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          y="7"
+          width="16"
+          height="2"
+          rx="1"
+          className={`transform origin-center transition duration-300 ease-out ${
+            isOpen && '!rotate-180'
+          }`}
+        />
+        <rect
+          y="7"
+          width="16"
+          height="2"
+          rx="1"
+          className={`transform origin-center rotate-90 transition duration-300 ease-out ${
+            isOpen && '!rotate-180'
+          }`}
+        />
+      </svg>
+    </div>
   )
 }
 export default ButtonSVG

ce9e4933137530613e7e14e2168fc210e8c192b6
JuanOros <juenas@hotmail.com>
2024-02-12 16:08:24 -0300

    :sparkles: adiciona menu com responsividade, focado para mobile

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -3,10 +3,15 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { Link as ReactLink } from 'react-scroll'
+import Bars from './icons/Bars'
+import { useState } from 'react'
+import Close from './icons/Close'
 
 function NavBar() {
+  const [menuOn, setMenu] = useState(false)
+
   return (
-    <div className="flex justify-between items-center w-full max-w-[1600px] pb-8 pt-0">
+    <div className="flex justify-between items-center w-full max-w-[1600px] pb-8 pt-0 relative">
       <Link href="/">
         <Image
           className="w-44 md:w-52"
@@ -17,7 +22,7 @@ function NavBar() {
         />
       </Link>
 
-      <ul className="hidden lg:flex gap-12">
+      <ul className={`${menuOn ? 'hidden' : ''} hidden lg:flex gap-6`}>
         <li className="text-jc-light-yellow px-4 pt-4 pb-2 rounded-lg hover:bg-white hover:bg-opacity-10">
           <Link href="/quem-somos">Quem somos</Link>
         </li>
@@ -39,6 +44,44 @@ function NavBar() {
         </li>
         <li className="text-jc-light-yellow p-4 rounded-lg hover:bg-white hover:bg-opacity-10">
           <Link href="/">Home</Link>
+        </li>
+      </ul>
+
+      <button onClick={() => setMenu(!menuOn)} className="lg:hidden">
+        {menuOn ? <Close /> : <Bars />}
+      </button>
+
+      <ul
+        className={`${menuOn ? '' : 'hidden'} lg:hidden flex flex-col absolute top-16 w-full p-5 rounded-xl pt-10 pb-10 backdrop-blur-md`}
+      >
+        <li className="bg-jc-light-yellow mb-5 text-center pt-3 pb-2 rounded-full text-jc-dark-blue font-semibold">
+          <Link onClick={() => setMenu(!menuOn)} href="/quem-somos">
+            Quem somos
+          </Link>
+        </li>
+        <li className="bg-jc-light-yellow mb-5 text-center pt-3 pb-2 rounded-full text-jc-dark-blue font-semibold">
+          <Link onClick={() => setMenu(!menuOn)} href="/como-funciona">
+            Como funciona
+          </Link>
+        </li>
+        <li className="bg-jc-light-yellow mb-5 text-center pt-3 pb-2 rounded-full text-jc-dark-blue font-semibold">
+          <ReactLink
+            onClick={() => setMenu(!menuOn)}
+            activeClass="active"
+            to="contact-component"
+            spy={true}
+            smooth={true}
+            offset={-50}
+            duration={500}
+            href="/"
+          >
+            Fale conosco
+          </ReactLink>
+        </li>
+        <li className="bg-jc-light-yellow mb-5 text-center pt-3 pb-2 rounded-full text-jc-dark-blue font-semibold">
+          <Link onClick={() => setMenu(!menuOn)} href="/">
+            Home
+          </Link>
         </li>
       </ul>
     </div>
---
+++ src/components/Header/icons/Close.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Close() {
+  return (
+    <svg
+      className="w-12 h-12 text-jc-light-yellow lg:hidden"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 18 18 6m0 12L6 6"
+      />
+    </svg>
+  )
+}
+
+export default Close

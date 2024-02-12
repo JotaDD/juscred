fbcb8d55cc6532849fa27a38649bc27a41c77330
JuanOros <juenas@hotmail.com>
2024-02-12 12:04:00 -0300

    :bug: adiciona responsividade para o componente InformationContent

--- src/components/InformationContent/TextImg.jsx
+++ src/components/InformationContent/TextImg.jsx
@@ -7,9 +7,9 @@ function TextImg({ info }) {
 
   return (
     <div
-      className={`flex ${startLeft ? 'flex-row-reverse' : ''} items-center h-[16rem] mgs:h-[24rem] mgs:border-none rounded-xl shadow-lg bg-zinc-100 mgs:shadow-none mgs:bg-white`}
+      className={`flex ${startLeft ? 'flex-row-reverse' : ''} items-center mgs:h-[24rem] mgs:border-none rounded-xl shadow-lg bg-zinc-100 mgs:shadow-none mgs:bg-white`}
     >
-      <p className="mgs:w-1/2 w-[99%] py-8 px-12 text-jc-dark-blue">{`${text}`}</p>
+      <p className="mgs:w-1/2 w-[99%] mgs:py-8 p-5 mgs:px-12 text-jc-dark-blue">{`${text}`}</p>
       <div
         className={`hidden mgs:block bg-[url(/${`${image}`})] bg-zinc-600 h-full mgs:w-full max-w-[50%] bg-cover bg-center`}
       ></div>

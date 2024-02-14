2f3527a33cd3a674b9c192a0b04abbebfaef9103
JotaDD <joaoeduardogomes@gmail.com>
2024-02-13 22:16:05 -0300

    style: adjust InformationComponent component

--- src/components/InformationContent/index.jsx
+++ src/components/InformationContent/index.jsx
@@ -3,7 +3,7 @@ import Content from './Content'
 
 function InformationContent({ title, contentArray }) {
   return (
-    <div className="w-screen bg-white px-10 py-20 mgs:p-24 mb-0">
+    <div className="w-screen bg-white px-1  mgs:px-24 mgs:pb-24 mb-0">
       <div className="max-w-[1600px] m-auto flex flex-col mgs:px-12">
         <h1 className="text-jc-dark-blue w-full text-center text-3xl">
           {`${title}`}

dfed796a03e42bfd9d8ce2d24d0799d873a56620
JotaDD <joaoeduardogomes@gmail.com>
2024-02-18 14:42:15 -0300

    chore: remove semicolons

--- src/components/InformationContent/TextImg.jsx
+++ src/components/InformationContent/TextImg.jsx
@@ -7,7 +7,7 @@ function TextImg({ info }) {
 
   return (
     <div
-      className={`flex ${startLeft ? 'flex-row-reverse' : ''} items-center mgs:h-[24rem] mgs:border-none rounded-xl shadow-lg bg-zinc-100 mgs:shadow-none mgs:bg-white`}
+      className={`flex ${startLeft ? 'flex-row-reverse' : ''} items-center mgs:h-[24rem] mgs:border-none rounded-xl shadow-lg bg-white mgs:shadow-none mgs:bg-white`}
     >
       <p className="mgs:w-1/2 w-[99%] mgs:py-8 p-5 mgs:px-12 text-jc-dark-blue">{`${text}`}</p>
       {/* <div

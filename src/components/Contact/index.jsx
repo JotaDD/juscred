a5f4b7d7a1ea2430503faf42bfc58b6b29beb848
JotaDD <joaoeduardogomes@gmail.com>
2024-02-19 13:34:30 -0300

    refactor: contact sizing

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,9 +6,9 @@ function Contact() {
   return (
     <div
       id="contact-component"
-      className="w-full bg-jc-dark-blue flex justify-between items-center p-12 md:p-24"
+      className="w-full border bg-jc-dark-blue flex justify-center items-center p-12 md:p-24"
     >
-      <div className="max-w-[1300px] xl:mx-[5%] xl2:mx-[10%] flex w-full flex-col lg:flex-row justify-between items-center">
+      <div className="max-w-[1200px] w-[80%] border border-blue-300 xl:mx-[5%] xl2:mx-[20%] flex flex-col lg:flex-row justify-between items-center">
         <Title />
         <Form />
       </div>
